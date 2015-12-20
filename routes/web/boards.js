c48e3dda97a975fc4ba5492b7b0e01c6d8683534
Luther Baker <luther@lutherbaker.com>
2015-12-20 13:23:40 -0600

    Moved admin.jade to boards.jade for now.

--- routes/web/boards.js
+++ routes/web/boards.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('web/admin', { title: 'Kanban | Fuzzy Bearings' });
+	res.render('web/boards', { title: 'Kanban | Fuzzy Bearings' });
 });
 
 module.exports = router;
